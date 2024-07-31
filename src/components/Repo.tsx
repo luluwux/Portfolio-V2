438e8af2cf4b40b8691444fb7ad69e5af52e69b5
Lulu <87658293+luluwux@users.noreply.github.com>
2024-07-31 03:27:50 +0300

    Update Repo.tsx

--- src/components/Repo.tsx
+++ src/components/Repo.tsx
@@ -56,6 +56,7 @@ function Repositories() {
                             {projects.length > 0 ? (
                                 projects.map((project: any) => (
                                     <div
+                                        key={project.id} 
                                         className="flex flex-col relative p-4 text-foreground m-1.5 h-[20vh] bg-secondary rounded-lg border border-foreground/5 hover:border-foreground/20 duration-300 justify-between hover:scale-105 "
                                         onClick={() => openGithubProject(project.html_url)}
                                     >
@@ -104,43 +105,3 @@ function Repositories() {
 }
 
 export default Repositories;
-
-
-export const projects = [
-    {
-        title: "Stripe",
-        description:
-            "A technology company that builds economic infrastructure for the internet.",
-        link: "https://stripe.com",
-    },
-    {
-        title: "Netflix",
-        description:
-            "A streaming service that offers a wide variety of award-winning TV shows, movies, anime, documentaries, and more on thousands of internet-connected devices.",
-        link: "https://netflix.com",
-    },
-    {
-        title: "Google",
-        description:
-            "A multinational technology company that specializes in Internet-related services and products.",
-        link: "https://google.com",
-    },
-    {
-        title: "Meta",
-        description:
-            "A technology company that focuses on building products that advance Facebook's mission of bringing the world closer together.",
-        link: "https://meta.com",
-    },
-    {
-        title: "Amazon",
-        description:
-            "A multinational technology company focusing on e-commerce, cloud computing, digital streaming, and artificial intelligence.",
-        link: "https://amazon.com",
-    },
-    {
-        title: "Microsoft",
-        description:
-            "A multinational technology company that develops, manufactures, licenses, supports, and sells computer software, consumer electronics, personal computers, and related services.",
-        link: "https://microsoft.com",
-    },
-];
